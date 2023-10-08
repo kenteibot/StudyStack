b7235131a3f9af561dd7f0f485798abb9b6e4ce4
AkihikoKen <mamedovkenanera@gmail.com>
2023-10-08 23:31:27 +0300

    измененил структуру классов

--- StudyStack/Lecture.cpp
+++ StudyStack/Lecture.cpp
@@ -1,9 +1,9 @@
 #include "Lecture.h"
+#include "FolderManager.h"
 
 Lecture::Lecture(String^ name, Subject^ owner)
 {
 	this->name = name;
 	this->owner = owner;
 	this->creationDate = DateTime::Now;
-	this->folderPath = "0";
 }
--- StudyStack/Task.cpp
+++ StudyStack/Task.cpp
@@ -4,6 +4,5 @@ Task::Task(String^ name, Subject^ owner)
     : Lecture(name, owner)
 {
     this->progress = 0;
-    this->folderPath = "1";
 }
 
